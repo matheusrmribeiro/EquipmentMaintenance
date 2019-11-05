c92dcb789396432cce42218f0df674b64c0ddd82
Matheus Ribeiro Miranda <43149218+matheusrmribeiro@users.noreply.github.com>
2019-11-05 14:00:50 -0300

    Layout Improvements
    * Login layout improvement

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:qrcode/bloc/blocAuth.dart';
 import 'package:qrcode/bloc/blocLogin.dart';
 
@@ -15,7 +16,6 @@ class LoginPage extends StatelessWidget {
       children: <Widget>[
         Material(
           child: Container(
-            padding: EdgeInsets.all(16.0),
             child: SafeArea(
               child: Form(
                 key: formKey,
@@ -25,23 +25,18 @@ class LoginPage extends StatelessWidget {
                     Container(
                       padding: EdgeInsets.only(top: 60.0),
                       child: Center(
-                        child: Text("Cascan", 
+                        child: Text("LogScan", 
                           style: TextStyle(
-                            color: Colors.purple, 
+                            color: Theme.of(context).primaryColor, 
                             fontSize: 50.0,
                           )
                         ),
                       )
                     ),              
                     Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Fields(), 
-                          SubmitButtons()
-                        ],
-                      )
+                      child: Fields(),
                     ),
+                    SubmitButtons()
                   ]
                 ),
               )
@@ -59,34 +54,55 @@ class Fields extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Container(
-          color: Colors.black.withOpacity(0.6),
-          padding: EdgeInsets.all(8.0),
-          child: TextFormField(
-            key: Key('email'),
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              icon: Icon(Icons.email)
-            ),
-            validator: BlocLogin.validateEmail,
-            onSaved: (value) => bloc.inEmail.add(value),
-          )
+        Card(
+          elevation: 6,
+          margin: EdgeInsets.fromLTRB(15, 5, 15, 5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(15, 2, 2, 2),
+            child: TextFormField(
+              key: Key('email'),
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                icon: Icon(Icons.email),
+                border: InputBorder.none,
+              ),
+              style: TextStyle(
+                fontSize: 18
+              ),
+              validator: BlocLogin.validateEmail,
+              onSaved: (value) => bloc.inEmail.add(value),
+            )
+          ),
         ),
-        Container(padding: EdgeInsets.only(top: 10.0),),
-        Container(
-          color: Colors.black.withOpacity(0.6),
-          padding: EdgeInsets.all(8.0),
-          child: TextFormField(
-            key: Key('password'),
-            decoration: InputDecoration(
-              labelText: 'Senha',
-              icon: Icon(Icons.lock_outline)
+        Card(
+          elevation: 6,
+          margin: EdgeInsets.all(15),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(15, 2, 2, 2),
+            child: TextFormField(
+              key: Key('password'),
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                icon: Icon(Icons.lock_outline),
+                border: InputBorder.none,
+              ),
+              style: TextStyle(
+                fontSize: 18
+              ),
+              obscureText: true,
+              validator: BlocLogin.validatePassword,
+              onSaved: (value) => bloc.inPassword.add(value),
             ),
-            obscureText: true,
-            validator: BlocLogin.validatePassword,
-            onSaved: (value) => bloc.inPassword.add(value),
           )
         ),
       ]
@@ -95,8 +111,6 @@ class Fields extends StatelessWidget {
 }
 
 class SubmitButtons extends StatelessWidget {
-  /*SubmitButtons(this._formKey);
-  final _formKey;*/
   final bloc = BlocProvider.getBloc<BlocLogin>();
   final blocAuth = BlocProvider.getBloc<BlocAuth>();
   
@@ -128,50 +142,36 @@ class SubmitButtons extends StatelessWidget {
     return StreamBuilder(
       stream: bloc.outType,
       builder: (context, snapshot){
-        if (snapshot.hasData) {
-          if (snapshot.data == ActionType.atLogin) {
-          return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Container(margin: EdgeInsets.only(top: 50.0),),
-                RaisedButton(
-                  key: Key('signIn'),
-                  color: Theme.of(context).primaryColor,
-                  elevation: 10.00,
-                  child: Text('Entrar', style: TextStyle(fontSize: 20.0)),
-                  onPressed: (){
-                    validateAndSubmit(snapshot.data);
-                  },
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
-                ),
-                FlatButton(
-                  child: Text('Criar uma conta', style: TextStyle(fontSize: 20.0)),              
-                  onPressed: moveToRegister,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
-                ),
-              ],
-            );
-          } 
-          else {
-            return Column(
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text('Criar uma conta', style: TextStyle(fontSize: 20.0)),
-                    onPressed: (){
-                      validateAndSubmit(snapshot.data);
-                    },
-                  ),
-                  FlatButton(
-                    child: Text('Já tenho uma conta', style: TextStyle(fontSize: 20.0)),
-                    onPressed: moveToLogin,
-                  ),
-                ],
-              );
-          }
-        }
-        else
+        if (!snapshot.hasData)
           return Container();
+        
+        return Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              width: 200,
+              child: RaisedButton(
+                key: Key('signIn'),
+                color: Theme.of(context).primaryColor,
+                elevation: 10.00,
+                child: Text('Entrar', 
+                  style: TextStyle(
+                    fontSize: 20.0, 
+                    color: Colors.white)
+                  ),
+                onPressed: (){
+                  validateAndSubmit(snapshot.data);
+                },
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
+              ),
+            ),
+            FlatButton(
+              child: Text('Criar uma conta', style: TextStyle(fontSize: 20.0)),              
+              onPressed: moveToRegister,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))
+            ),
+          ],
+        );
       },
     );
     
